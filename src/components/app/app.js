3341bbf0a5c503bcfa76d809c35f581976e9c586
Rymney34 <locktimil70@gmail.com>
2025-07-29 17:22:46 +0100

    New Project realted to Marvel and working with server , working with APIs

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>GG New Project realted to Marvel and working with server , working with APIs</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from '../src/components/app/app';
+import reportWebVitals from './reportWebVitals';
+import MarvelService from './services/MarvelService'
+
+const marvelService = new MarvelService();
+
+marvelService.getCharacter(2).then(res=> console.log(res));
+
+// marvelService.getAllCharacters().then(res=> res.data.results.forEach(item => console.log(item.name)));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
