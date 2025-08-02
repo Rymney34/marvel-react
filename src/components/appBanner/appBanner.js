0328e5f39321b021b29aa7a72c9ee2c1f5727cc0
Rymney34 <locktimil70@gmail.com>
2025-08-02 17:44:32 +0100

    Banner is nearly done

--- src/components/appBanner/appBanner.js
+++ src/components/appBanner/appBanner.js
@@ -1,29 +1,48 @@
+import './appBanner.css'
+
+import char from '../../resources/img/thor.jpeg'
+
+import imgB from '../../resources/img/mjolnir.png'
+
 const AppBanner = () => {
     return (
-        <div className="bannerBlock">
-            <div className="randomInfoBlock">
-                <img className="charImg" alt="Char image"/>
-                <h2 className="charName">Thor</h2>
-                <p></p>
-                <div className="buttonsBlock">
-                    <div className="button1">
-
+        <div className="bannerWrapper">
+            <div className="bannerBlock">
+                <div className="randomInfoBlock">
+                    <img className="charImg" alt="Char" src={char}/>
+                    <div className='charDetails'>
+                        <h2 className="charName"><b>Thor</b></h2>
+                        <p className="charDesc"> As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted ham
+                            mer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
+                        </p>
+                        <div className="buttonsBlock">
+                            <div className="button1">
+                                <a>Homepage</a>
+                            </div>
+                            <div className="button1">
+                                <a>wiki</a>
+                            </div>
+                        </div>
                     </div>
+                    
                 </div>
-            </div>
-            <div className="randomCTABlock">
-                <p>
-                    Random character for today!
-                    Do you want to get to know him better?
-                </p>
-                <p>
-                    Or choose another one
-                </p>
-                <button>
-
-                </button>
-                <div className="imageBlock">
-                    <img className="img" alt='decoration'/>
+                <div className="randomCTABlock">
+                    <p>
+                        Random character for today!<br></br>
+                        
+                        Do you want to get to know him better?
+                    </p>
+                    <p>
+                        Or choose another one
+                    </p>
+                    <div className="buttonTry">
+                            <div className="button1">
+                                <a>tryit</a>
+                            </div>
+                    </div>
+                    <div className="imageBlock">
+                        <img className="img" src={imgB} alt='decoration'/>
+                    </div>
                 </div>
             </div>
         </div>
